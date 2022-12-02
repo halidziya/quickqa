e33801cc588621b923f863f5eeadef4fe3e12633
Halid Ziya Yerebakan <halidziya@gmail.com>
2022-12-02 17:34:48 -0500

    New sentence transformers

--- qa.py
+++ qa.py
@@ -1,13 +1,27 @@
 from transformers import pipeline
 from flask import Flask, request
-p = pipeline('question-answering')
+from nltk.tokenize import sent_tokenize
+import numpy as np
 
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-mpnet-base-v2')
 with open('content.txt') as fil:
     context = fil.read()
 
-#p(question="what can I do?", context=context)
+sentences = sent_tokenize(context)
+embeddings = model.encode(sentences)
 
 
+def old_version():
+    p = pipeline('question-answering')
+
+
+
+    #p(question="what can I do?", context=context)
+
+    @app.route("/ask", methods=['GET'])
+    def ask():
+        return p(question=request.args.get("q"), context=context)['answer']
 
 
 app = Flask(__name__)
@@ -18,6 +32,12 @@ def main():
         page = fil.read()
     return page
 
+
 @app.route("/ask", methods=['GET'])
 def ask():
-    return p(question=request.args.get("q"), context=context)['answer']
+    question=request.args.get("q")
+    encodedq = model.encode([question])
+    distances = np.linalg.norm(embeddings - encodedq, axis=1)
+    closest = np.argsort(distances)[:2]
+    return sentences[closest[0]] + '\n' + sentences[closest[1]]
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+flask --app qa run
